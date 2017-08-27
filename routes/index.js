9fa6d32bc87c65291d19137f894c5bd3bfa920c4
Renan Arimathea <renanthea@hotmail.com>
2017-08-27 03:38:00 -0300

    API VISA COMPLETE MERGED WITH API AGILLITAS

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var CLIENT_ID_AGILLITAS = '9dbdbd34-56f6-3cd2-8e3a-5c245a54ccf8'              //
 var CLIENT_SECRET_AGILLITAS = 'dd3b043e-4b1a-3166-a980-bb3de5fdcc57'          // ID para conceder autorizacao para request http
 var ACCESS_TOKEN_AGILLITAS = '64b932c2-4f6c-3625-846d-1376348abb73'           // ID de user para conceder autorizacao para request http a um usuario
 
+
 module.exports = function(app){
 
   // Importa modulos nativos
@@ -14,10 +15,149 @@ module.exports = function(app){
 
   // Custom Modules
   var db = require('./../libs/connectdb.js')()
+  
+  // Modulos da VISA
+  var VisaAPIClient = require('./../libs/visaapiclient.js');
+  var config = require('./../configs/configuration.json');
 
   app.get("/", function(req, res){
     res.send('aeee man');
   })
+
+  /*
+    ### MERGE VISA CHECKOUT => AGILLITAS ###
+  */
+
+  app.get('/cybersource_agillitas_sales/:idCartao/:valor', (req, res) => {
+
+    return new Promise(function(resolve, reject){
+
+      var idCartao = req.params.idCartao
+      var valor = req.params.valor
+
+      var visaAPIClient = new VisaAPIClient();
+
+      var paymentAuthorizationRequest = JSON.stringify({
+        "amount": valor,
+        "currency": "USD",
+        "payment": {
+          "cardNumber": idCartao,
+          "cardExpirationMonth": "10",
+          "cardExpirationYear": "2030"
+        }
+      })
+
+      var apiKey = config.apiKey;
+      var baseUri = 'cybersource/';
+      var resourcePath = 'payments/v1/sales';
+      var queryParams = 'apikey=' + apiKey;
+
+      visaAPIClient.doXPayRequest(baseUri, resourcePath, queryParams, paymentAuthorizationRequest, 'POST', {}, function(err, responseCode) {
+        if(err) {
+          // assert.equal(responseCode, 201);
+           console.log(err)
+        } 
+        else {
+          // assert(false);
+         
+          console.log('Autorizacao VISA efetuada com sucesso!')
+          console.log(responseCode)
+          console.log('Iniciando processamento na Agillitas ...')
+          console.log('====================================')
+
+
+          var url = 'https://api-visa.sensedia.com/sandbox/visa/agillitas/v1/cartoes/' + idCartao + "/saldo"
+
+          // Configura o request
+          var requestOptions = {
+            uri : url,
+            method: 'PUT',
+            resolveWithFullResponse : true,
+            timeout: 120000,
+            headers: {
+              access_token:   ACCESS_TOKEN_AGILLITAS,
+              client_id:      CLIENT_ID_AGILLITAS
+            },
+            body: {
+              "saldo" : {
+                "valor": valor
+              }
+            },
+            json: true
+          }
+
+          console.log(requestOptions)
+          console.log('Processando valor ' + valor)
+          console.log('====================================')
+
+          // Envia o request
+          rp(requestOptions).then(function(response){
+
+            var body = response.body
+
+            console.log('Processamento na Agillitas finalizada !')
+            console.log('statusCode: ' + response.statusCode)
+            console.log('====================================')
+            console.log(body)
+
+            resolve(body)
+
+            res.send('Operacao realizada com sucesso')
+
+          })
+
+        }
+        
+      });
+
+    })
+
+  })
+
+  /*
+    ### REQUESTS FROM API VISA CHECKOUT ###
+  */
+
+  app.get('/cybersource_sales_add/:cardNumber/:amount', (req, res) => {
+
+    var cardNumber = req.params.cardNumber;
+    var amount = req.params.amount
+
+    var visaAPIClient = new VisaAPIClient();
+
+    var paymentAuthorizationRequest = JSON.stringify({
+      "amount": amount,
+      "currency": "USD",
+      "payment": {
+        "cardNumber": cardNumber,
+        "cardExpirationMonth": "10",
+        "cardExpirationYear": "2030"
+      }
+    })
+
+    var apiKey = config.apiKey;
+		var baseUri = 'cybersource/';
+		var resourcePath = 'payments/v1/sales';
+		var queryParams = 'apikey=' + apiKey;
+
+		visaAPIClient.doXPayRequest(baseUri, resourcePath, queryParams, paymentAuthorizationRequest, 'POST', {}, function(err, responseCode) {
+      if(!err) {
+        // assert.equal(responseCode, 201);
+        console.log('Autorizacao VISA efetuada com sucesso!')
+        console.log(responseCode)
+      } 
+      else {
+        // assert(false);
+        console.log(err)
+      }
+
+		});
+
+  })
+
+  /*
+    ### REQUESTS FROM API AGILLITAS ###
+  */
 
   // Associa o proxy de um cartão para um portador
 
@@ -244,47 +384,5 @@ module.exports = function(app){
   //   res.render('facebook_auth')
   // })
 
-  // app.get('/mercado', function(req, res){
-  //   res.render('mercado')
-  // })
-
-  // app.get('/preco', function(req, res){
-  //   res.render('preco')
-  // })
-
-  // app.get('/receita', function(req, res){
-  //   res.render('receita')
-  // })
-
-  // app.get('/pesquisa', function(req, res){
-  //   res.render('pesquisa')
-  // })
-
-
-
-  app.get('/get_beer/:beerName', function(req, res){
-
-    var beerName = req.params.beerName
-
-    var m_Brejas = require('../crowler/brejas.js')
-
-    m_Brejas.getBeersByName(beerName).then(function(result){
-
-      res.send(result)
-
-    })
-
-  })
-
-  app.get('/get_beer_static/stella', function(req, res){
-
-    var renanzinho = {
-      nome: 'Stella',
-      preco: '6,50'
-    }
-
-    res.send(renanzinho)
-
-  })
 
 }
